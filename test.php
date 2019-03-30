1deea021398b2833de0f22ad14388a46f26ff8c2
lanied <hmaforsiv@gmail.com>
2019-03-30 00:21:10 -0600

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,16 @@
 <?php
+$time = microtime(true);
 
-$conexion = mysqli_connect('db4free.net', 'intellidesk', 'aeroalpesSIA') or die('Error: ' . mysql_error());
+$conexion = mysqli_connect('ricky.heliohost.org', 'sia2019', 'intellideskSIA') or die('Error: ' . mysql_error());
 echo 'Conexion exitosa';
-mysqli_select_db($conexion, "sia_db") or die('No se pudo seleccionar la base de datos');
+mysqli_select_db($conexion, "sia2019_db") or die('No se pudo seleccionar la base de datos');
+
+$con_time = microtime(true);
 
 $query = 'SELECT * FROM Avion';
 $resultado = mysqli_query($conexion, $query) or die('Consulta fallida: ' . mysqli_error($conexion));
+
+$sel_time = microtime(true);
 
 echo "<table border= '2'\n";
 while ($registro = mysqli_fetch_array($resultado, MYSQLI_ASSOC)) 
@@ -21,4 +26,8 @@ echo "</table>\n";
 
 mysqli_free_result($resultado);
 mysqli_close($conexion);
+
+printf("Connect time: %f\nQuery time: %f\n",
+       $con_time-$time,
+       $sel_time-$con_time);
 ?>
