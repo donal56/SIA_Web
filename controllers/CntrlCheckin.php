8769fb19221920da072599729e7f5180f19a75d7
lanied <hmaforsiv@gmail.com>
2019-04-20 01:02:40 -0500

    Jquery php
    $_SERVER['DOCUMENT_ROOT']  usado para que los path no sean relativos , ya que al llamarlos desde jquery este toma la ruta del archivo phtml como base del path

--- controllers/CntrlCheckin.php
+++ controllers/CntrlCheckin.php
@@ -1,15 +1,15 @@
 <?php
-if(!$_GET){
 	//call model
-	require_once("models/Boletos.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/models/database.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/models/Boletos.php");
 	$moBoleto = new moBoletos();
-	//call view
-	require_once("views/Checkin.phtml");
-}else{	
-	echo $_GET["numBoleto"]." hola";
-/*$data = $moBoleto->isValidID($_GET["numBoleto"]);
-	echo("<script>console.log('PHP: ".var_export($data,true)."');</script>"); */
 
+	if(!$_GET){
+		//call view
+		require_once("views/Checkin.phtml");
+	}else{
+		
+		echo var_export($moBoleto->isValidID($_GET["numBoleto"]),true);
 }
 	
 ?>
